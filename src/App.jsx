e761cf87e84e06e6dfd50bdda03ad5dd3b57c0ce
Asiful-0160 <asifulauishik@gmail.com>
2026-09-13 19:38:36 +0600

    set up React app with Vite and styling tools

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ToastContainer } from 'react-toastify'
+
+function App() {
+  return (
+    <>
+      <main className="p-6">
+        <h1 className="text-3xl font-bold">Dev Stack Builder</h1>
+        <p className="mt-3">Explore development technologies and build your ideal stack.</p>
+      </main>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
